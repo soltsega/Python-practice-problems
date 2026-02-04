e598aaada6085cc493d2e14ff3b5260656a06cfa
soltsega <tsegasolomon538@gmail.com>
2026-02-04 21:45:45 +0300

    Added practice files

---
+++ functions/sleepFun.py
@@ -0,0 +1,17 @@
+import time
+def multiply(a, b):
+    print(f"the product is {a*b}")
+    time.sleep(2)
+    print("Thank you")
+
+multiply(3,4)
+
+print(1,2,3,4, sep = '-')
+# arguments in python
+# They can be of 4 types
+# 1. positional
+# 2. default arguments
+# 3. keyword arguments
+# 4. arbitrary arguments
+
+# 1. 
