d2025b0ee3d1a73bc65889cc8722ecb5587861e3
soltsega <tsegasolomon538@gmail.com>
2026-02-06 15:48:28 +0300

    Default dictionaries practice

---
+++ functions/defaultdict.py
@@ -0,0 +1,31 @@
+# Defaultdict in python
+# They are used to provide default values for missing keys
+# They are similar to regular dictionaries, but they have a default value for missing keys
+# They are imported from the collections module
+
+
+from collections import defaultdict
+
+def findWinners(matches):
+    loss_count = defaultdict(int)
+    players = set()
+
+    for winner, loser in matches:
+        players.add(winner)
+        players.add(loser)
+        loss_count[loser] += 1
+
+    zero_losses = []
+    one_loss = []
+
+    for player in players:
+        if loss_count[player] == 0:
+            zero_losses.append(player)
+        elif loss_count[player] == 1:
+            one_loss.append(player)
+
+    zero_losses.sort()
+    one_loss.sort()
+
+    return [zero_losses, one_loss]
+
